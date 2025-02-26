2113ee601cb09c89bee7d22d29ab330c89b8c780
sjlee9908 <sjlee9908@naver.com>
2025-02-26 16:27:51 +0900

    Initial commit

---
+++ src/test/java/com/anam145/anamwallet/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anam145.anamwallet.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
